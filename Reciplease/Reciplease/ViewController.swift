e8f18a46f6cf3927e7ce21a9b7f731cfacbf2457
jonjonpir <jonathan.pirot@capgemini.com>
2022-12-09 10:47:31 +0100

    start project

---
+++ Reciplease/Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by jonathan P on 09/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
