1113628ee69b72587032eab423d81a4a4075c144
jonjonpir <jonathan.pirot@capgemini.com>
2022-12-09 13:44:23 +0100

    add asset

---
+++ Reciplease/Reciplease/Controllers/TapBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TapBarController.swift
+//  Reciplease
+//
+//  Created by jonathan P on 09/12/2022.
+//
+
+import UIKit
+
+class TapBarController: UITabBarController {
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+  private func setupTapBar() {
+
+  }
+}
